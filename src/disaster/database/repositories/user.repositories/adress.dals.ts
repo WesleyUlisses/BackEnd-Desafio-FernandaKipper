a8dbbd41709458c2cd75e62815913ee2b9c8bf7d
bruno34154 <www.brunomesenga@gmail.com>
2024-08-03 22:27:48 -0300

    adding

--- src/disaster/database/repositories/user.repositories/adress.dals.ts
+++ src/disaster/database/repositories/user.repositories/adress.dals.ts
@@ -1,32 +1,55 @@
 import { prisma } from '../../prisma.databases';
 import { IAdressCreate } from '../../../interfaces/adress.interfaces';
+import { CityDals } from './city.dals';
 
 class AdressDals {
-    constructor() {}
+    private cityDals: CityDals;
+    constructor() {
+        this.cityDals = new CityDals();
+    }
+    async findAddressesByCity(cityName: string, state: string) {
+        // Verificar se a cidade existe
+        const city = await this.cityDals.doesCityExist(cityName, state);
+        if (!city) {
+            throw new Error(`Cidade ${cityName}, ${state} não encontrada no banco de dados.`);
+        }
+
+        // Recuperar todos os endereços associados à cidade
+        const addresses = await prisma.adress.findMany({
+            where: {
+                cityId: city.id,
+            },
+        });
+
+        return addresses;
+    }
 
     async createAdress({ neighborhood, cep, cityName, latitude, longitude, state, userId }: IAdressCreate) {
         const result = await prisma.$transaction(async (prisma) => {
-            // Primeiro, crie a cidade
-            const createdCity = await prisma.city.create({
-                data: {
-                    name: cityName,
-                    state: state,
-                    latitude: latitude,
-                    longitude: longitude,
-                },
-            });
+
+            let city = await this.cityDals.doesCityExist(cityName, state);
+            if(!city){
+                city = await prisma.city.create({
+                    data: {
+                        name: cityName,
+                        state: state,
+                        latitude: latitude,
+                        longitude: longitude,
+                    },
+                });
+         }
 
             // Em seguida, crie o endereço relacionado à cidade
             const createdAddress = await prisma.adress.create({
                 data: {
                     neighborhood,
                     cep,
-                    cityId: createdCity.id, // Relacionando com a cidade criada
+                    cityId: city.id, 
                     userId,
                 },
             });
 
-            return { createdCity, createdAddress };
+            return { city, createdAddress };
         });
 
         return result;
--- src/disaster/services/user.services.ts
+++ src/disaster/services/user.services.ts
@@ -1,41 +1,109 @@
-import { UserDals } from '../database/repositories/user.repositories/user.dals';
-import { AdressDals } from '../database/repositories/user.repositories/adress.dals';
-import { IUserData } from '../interfaces/user.interfaces';
-import { BadRequestError } from '../../helpers/error.helpers';
-
+import { UserDals } from "../database/repositories/user.repositories/user.dals";
+import { AdressDals } from "../database/repositories/user.repositories/adress.dals";
+import { IUserData } from "../interfaces/user.interfaces";
+import { BadRequestError } from "../../helpers/error.helpers";
+import axios  from "axios";
+import { CityDals } from "../database/repositories/user.repositories/city.dals";
 
 class UserServices {
-    private userDals: UserDals;
-    private adressDals: AdressDals;
+  private userDals: UserDals;
+  private adressDals: AdressDals;
+  private cityDals: CityDals;
 
-    constructor() {
-        this.userDals = new UserDals();
-        this.adressDals = new AdressDals();
+  constructor() {
+    this.userDals = new UserDals();
+    this.adressDals = new AdressDals();
+    this.cityDals = new CityDals();
+  }
+
+  async createUser({
+    name,
+    whatsapp,
+    phone,
+    email,
+    city,
+    state,
+    cep,
+    neighborhood,
+    latitude,
+    longitude,
+  }: IUserData) {
+    const user = await this.userDals.createUser({
+      name,
+      whatsapp,
+      phone,
+      email,
+    });
+    if (!user) {
+      throw new BadRequestError({ message: "user not created" });
+    }
+    const findCity = await this.cityDals.doesCityExist(city, state);
+    if(!findCity){
+        const trigger = await this.registerTrigger(Number(latitude), Number(longitude) );
+        console.log(trigger)
+    }
+    const address = await this.adressDals.createAdress({
+      cityName: city,
+      state: state,
+      userId: user.id,
+      cep: cep,
+      neighborhood: neighborhood,
+      latitude: latitude,
+      longitude,
+    });
+    if (!address) {
+      throw new BadRequestError({ message: "adress not created" });
     }
 
-    async createUser({ name, whatsapp, phone, email, city, state, cep, neighborhood, latitude, longitude}: IUserData) {
-            const user = await this.userDals.createUser({ name, whatsapp, phone, email });
-            if(!user){
-                throw new BadRequestError({message: 'user not created'})
-            }
-            const address = await this.adressDals.createAdress({
-                cityName: city,
-                state: state,
-                userId: user.id, 
-                cep: cep,
-                neighborhood: neighborhood,
-                latitude: latitude,
-                longitude
-            });
-            if(!address){
-                throw new BadRequestError({message: 'adress not created'})
-            }
-            return {
-                user: user,
-                address: address,
-                message: "User and address created successfully"
-            };
-    }
+    
+    
+    return {
+      user: user,
+      address: address,
+      message: "User and address created successfully",
+    };
+  }
+
+  private async registerTrigger(latitude: number, longitude: number) {
+    const apiKey = process.env.APIKEY;
+    const url = `http://api.openweathermap.org/data/3.0/triggers?appid=${apiKey}`;
+    const requestBody = {
+      time_period: {
+        start: {
+          expression: "after",
+          amount: 10,
+        },
+        end: {
+          expression: "after",
+          amount: 432000000,
+        },
+      },
+      conditions: [
+        {
+          name: "temp",
+          expression: "$gt",
+          amount: 280,
+        },
+      ],
+      area: [
+        {
+          type: "Point",
+          coordinates: [latitude, longitude],
+        },
+      ],
+    };
+
+    const response = axios
+      .post(url, requestBody)
+      .then((response: any) => {
+        return response.data;
+      })
+      .catch((error: any) => {
+        return error;
+      });
+
+      return response
+  }
 }
 
 export { UserServices };
